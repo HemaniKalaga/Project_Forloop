4f20101f63bb905d45370bd63156e591e6736932
HemaniKalaga <hemanikalaga002@gmail.com>
2024-01-12 13:28:34 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def sudheer1(request):
+    return render(request,'sudheer1.html')
+
+def sudheer2(request):
+    return render(request,'sudhee2r.html')
+
+def srujana(request):
+    return render(request,'srujana.html')
